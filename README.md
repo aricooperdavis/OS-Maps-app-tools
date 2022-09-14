92fe69c3bf2e943f781c559d0af6dbf20221b3a5
Ari Cooper-Davis <ari@cooper-davis.net>
2022-09-14 15:59:05 +0100

    Add dedupe functionality

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Please don't share any OS Maps - purchasing their excellent maps funds the work 
 Run with the `--help` flag for the command line help text:
 
 ```powershell
-python ostools.py --help
+> python .\ostools.py --help
 usage: ostools.py [-h] [-verbose] command ...
 
 Tools for working with maps from the OS Maps app.
@@ -29,6 +29,7 @@ positional arguments:
   command
     extract   Extract maps from mbgl-offline.db
     convert   Convert MBTiles files from png to webp
+    dedupe    Dedupe tiles from overlapping MBTiles files
 
 optional arguments:
   -h, --help  show this help message and exit
@@ -39,10 +40,10 @@ optional arguments:
 You should not need to install additional packages to `extract`, but the `convert` command requires [Pillow](https://pillow.readthedocs.io/en/stable/installation.html) and other undocumented commands require [requests](https://requests.readthedocs.io/en/latest/user/install/#install). You should be able to use the `requirements.txt` file to install these; i.e. `pip install -r ./requirements.txt`.
 
 ### Extracting maps (`extract`)
-_You'll need a rooted device to `extract`. If you don't have a rooted physical device then you can use the emulator built into the Android Studio SDK with a rooted virtual device._
+_You'll need a rooted android device to `extract`. If you don't have a rooted physical device then you can use the emulator built into the Android Studio SDK with a rooted virtual device._
 
 1. Within the OS Maps app, [download the maps](https://osmaps.com/os-maps-help?categoryId=631349&article=637593#article-id-637593) that you want to extract
-1. Copy `/data/data/uk.co.ordnancesurvey.osmaps/files/mbgl-offline.db` from the device to your computer
+1. Copy `/data/data/uk.co.ordnancesurvey.osmaps/files/mbgl-offline.db` from the android device to your computer. You can do this using ADB: `adb shell "su -c cat /data/data/uk.co.ordnancesurvey.osmaps/files/mbgl-offline.db" > ./mbgl-offline.db`
 1. Run `python ostools.py extract`
 
 Each region downloaded in the OS Maps app will be extracted into its own `.mbtiles` file.
@@ -89,34 +90,69 @@ Reference the [Pillow documentation](https://pillow.readthedocs.io/en/stable/han
 | 26,912 bytes | 23,668 bytes | 16,726 bytes | 14,476 bytes | 8,820 bytes | 2,362 bytes |
 | ![original tile](./resources/original.png) | ![compressed tile](./resources/100.webp) | ![compressed tile](./resources/75.webp) | ![compressed tile](./resources/50.webp) | ![compressed tile](./resources/25.webp) | ![compressed tile](./resources/0.webp) |
 
+### Deduping tiles ('dedupe')
+Adjacent OS maps overlap at the edges, but when using digital maps there is no need for this overlap, so storing these duplicated tiles is just a waste of space. As such the `dedupe` command allows you to remove tiles from one MBTiles file that appear in another.
+
+Note that these tiles are removed from the MBTiles file passed as the _first_ argument, and that this removal is performed _in place_.
+
+Once the duplicate tiles are removed the MBTiles database is [`vacuum`ed](https://www.sqlite.org/lang_vacuum.html) so that the file size saving may be obtained. Note that this process can require as much as twice the original file size as free disk space.
+
+There are no optional arguments for this command:
+
+```powershell
+> python .\ostools.py dedupe --help
+usage: ostools.py dedupe [-h] file1 file2
+
+positional arguments:
+  file1       path to MBTile to dedupe (in place)
+  file2       path to MBTile to compare against
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+
 ### Other commands
 Other undocumented commands are unsupported and should not be used.
 
 ## Example usage
-A simple example of extracting and converting a map from the OS Maps app database:
+A simple example of extracting, converting, and deduping two maps from the OS Maps app database:
 
 ```powershell
 > python .\ostools.py -verbose extract
-    Loading OS Maps database './mbgl-offline.db': Done
-    Identifying regions: Explorer OL44
-    Creating outfile 'Explorer OL44.mbtiles': Done
-    Preparing MBTiles structure: Done
-    Decompressing and copying map tiles: Done
-    Extract completed.
 
-> python .\ostools.py -verbose convert 'Explorer OL44.mbtiles'
-    Loading MBTiles file 'Explorer OL44.mbtiles': Done
-    Creating outfile 'Explorer OL44_webp.mbtiles': Done
-    Preparing MBTiles structure: Done
-    Converting tiles to WEBP (be patient!): Done
-    Convert completed.
+  Loading OS Maps database './mbgl-offline.db': Done
+  Identifying regions: Explorer OL7, Explorer OL6
+  Creating outfile 'Explorer OL7.mbtiles': Done
+  Preparing MBTiles structure: Done
+  Decompressing and copying map tiles: Done
+  Creating outfile 'Explorer OL6.mbtiles': Done
+  Preparing MBTiles structure: Done
+  Decompressing and copying map tiles: Done
+  Extract completed.
+
+> python .\ostools.py -verbose convert 'Explorer OL6.mbtiles'
+
+  Loading MBTiles file 'Explorer OL6.mbtiles': Done
+  Creating outfile 'Explorer OL6_webp.mbtiles': Done
+  Preparing MBTiles structure: Done
+  Converting tiles to WEBP (be patient!): Done
+  Convert completed.
+
+> python .\ostools.py -verbose dedupe 'Explorer OL6_webp.mbtiles' 'Explorer OL7.mbtiles'
+
+  Loading databases: Done
+  Removing tiles from 'Explorer OL6_webp.mbtiles' that appear in 'Explorer OL7.mbtiles': 696 duplicates
+  Resizing MBTiles database: Done
+  Dedupe completed.
 
 > ls *.mbtiles
-    Directory: .\OS-Maps-app-tools
-    Mode             LastWriteTime         Length Name
-    ----             -------------         ------ ----
-    -a----       00/00/0000  00:00       72069120 Explorer OL44.mbtiles
-    -a----       00/00/0000  00:00       50507776 Explorer OL44_webp.mbtiles
+
+  Directory: .\OS-Maps-app-tools
+  Mode         LastWriteTime       Length Name
+  ----         -------------       ------ ----
+  -a----    00/00/0000 00:00    147337216 Explorer OL7.mbtiles
+  -a----    00/00/0000 00:00    128802816 Explorer OL6.mbtiles
+  -a----    00/00/0000 00:00     76795904 Explorer OL6_webp.mbtiles
 ```
 
-Note the difference in file size once the map tiles are converted from `PNG` to `WEBP` formats.
+Note the difference in file size between the originally extracted Explorer OL6 map (128.8Mb) and the same once converted to WEBP and with duplicate tiles from the adjacent Explorer OL7 map removed (76.8Mb).
--- ostools.py
+++ ostools.py
@@ -37,22 +37,33 @@ def parse_args(no_conversion, no_update):
     subparsers = parser.add_subparsers(required=True, dest='command', metavar='command')
 
     extract_parser = subparsers.add_parser('extract', help='Extract maps from mbgl-offline.db')
-    extract_parser.add_argument('-file', type=str, default='./mbgl-offline.db', help='path to OS Maps database (default: \'%(default)s\')')
-    extract_parser.add_argument('-regions', nargs='*', type=str, help='regions to extract from the database')
+    extract_parser.add_argument('-file', default='./mbgl-offline.db', help='path to OS Maps database (default: \'%(default)s\')')
+    extract_parser.add_argument('-regions', nargs='*', help='regions to extract from the database')
     extract_parser.add_argument('-zoom', type=int, default=16, help='MBTiles tile zoom level (default: %(default)s)')
 
     if not no_conversion:
         convert_parser = subparsers.add_parser('convert', help='Convert MBTiles files from png to webp')
-        convert_parser.add_argument('file', type=str, help='path to MBTiles file')
+        convert_parser.add_argument('file', help='path to MBTiles file')
         convert_parser.add_argument('-quality', type=int, default=50, help='quality of WEBP compression (1-100, default: %(default)s)')
+
+    dedupe_parser = subparsers.add_parser('dedupe', help='Dedupe tiles from overlapping MBTiles files')
+    dedupe_parser.add_argument('file1', help='path to MBTile to dedupe (in place)')
+    dedupe_parser.add_argument('file2', help='path to MBTile to compare against')
 
     if not no_update:
         update_parser = subparsers.add_parser('update')
-        update_parser.add_argument('file', type=str, help='path to MBTiles file')
+        update_parser.add_argument('file', help='path to MBTiles file')
         update_parser.add_argument('-container', type=str, default='2021-12', help='update container (default: \'%(default)s)\'')
         update_parser.add_argument('-delay', type=float, default=1.0, help='seconds delay between tile-server requests (default: %(default)s)')
 
     return parser.parse_args()
+
+def flip_y(y, z=16):
+    """
+    Convert between XYZ and TMS notation for y coords
+    https://gist.github.com/tmcw/4954720
+    """
+    return math.floor((2**z)-y-1)
 
 def extract(file, rois, zoom, verbose):
     """
@@ -111,7 +122,12 @@ def extract(file, rois, zoom, verbose):
 
         # Create tiles table
         # https://github.com/mapbox/mbtiles-spec/blob/master/1.3/spec.md#schema-1
-        out_cur.execute('CREATE TABLE tiles (zoom_level integer, tile_column integer, tile_row integer, tile_data blob)')
+        out_cur.execute('''CREATE TABLE tiles (
+            zoom_level integer,
+            tile_column integer,
+            tile_row integer,
+            tile_data blob
+        )''')
         if verbose: print('Done')
 
         if verbose: print('Decompressing and copying map tiles: ', end='', flush=True)
@@ -124,7 +140,10 @@ def extract(file, rois, zoom, verbose):
             AND tiles.z == ?
         ''', [r_id, zoom]):
 
-            out_cur.execute('INSERT INTO tiles VALUES (?, ?, ?, ?)', [z, x, math.floor((2**z)-1-y), zlib.decompress(data)])
+            out_cur.execute(
+                'INSERT INTO tiles VALUES (?, ?, ?, ?)',
+                [z, x, flip_y(y, z), zlib.decompress(data)]
+            )
         if verbose: print('Done')
 
         # Close db
@@ -163,17 +182,59 @@ def convert(file, quality, verbose):
 
     # Converting tiles to webp
     if verbose: print('Converting tiles to WEBP (be patient!): ', end='', flush=True)
-    for [z, x, y, png] in in_cur.execute('SELECT zoom_level, tile_column, tile_row, tile_data FROM tiles'):
+    for [z, x, y, png] in in_cur.execute(
+            'SELECT zoom_level, tile_column, tile_row, tile_data FROM tiles'
+        ):
         webp = io.BytesIO()
         Image.open(io.BytesIO(png)).save(webp, format='webp', method=6, quality=quality)
         out_cur.execute('INSERT INTO tiles VALUES (?, ?, ?, ?)', [z, x, y, webp.getvalue()])
     if verbose: print('Done')
 
+    # Close DBs
     in_db.commit()
     in_db.close()
     out_db.commit()
     out_db.close()
     if verbose: print('Convert completed.')
+    return True
+
+def dedupe(file1, file2, verbose):
+    """
+    Remove (in place) duplicated tiles from overlapping MBTiles files.
+    """
+    if verbose: print('Loading databases: ', end='', flush=True)
+    # Load file to dedupe
+    dd_db = sqlite3.connect(file1)
+    dd_cur = dd_db.cursor()
+
+    # Attach comparison file
+    dd_cur.execute('ATTACH ? AS cp_db', [file2])
+    if verbose: print('Done')
+
+    # Dedupe
+    if verbose: print(f'Removing tiles from \'{file1}\' that appear in \'{file2}\': ', end='', flush=True)
+    dd_cur.execute(''' DELETE FROM main.tiles WHERE (
+        tile_row IN (
+            SELECT tile_row FROM cp_db.tiles
+        ) AND tile_column IN (
+            SELECT tile_column FROM cp_db.tiles
+        ) AND zoom_level IN (
+            SELECT zoom_level FROM cp_db.tiles
+        )
+    )''')
+
+    dupes = dd_cur.rowcount
+    if verbose: print(f'{dupes} duplicates')
+
+    # Close DBs
+    dd_db.commit()
+    if dupes > 0:
+        if verbose: print('Resizing MBTiles database: ', end='', flush=True)
+        dd_cur.execute('VACUUM')
+        if verbose: print('Done')
+    dd_db.close()
+    if verbose: print('Dedupe completed.')
+
     return True
 
 def update(file, delay, container, verbose):
@@ -198,16 +259,15 @@ def update(file, delay, container, verbose):
         if name == 'name':
             value = value[:-7]+container.replace('-','/')
         out_cur.execute('INSERT INTO metadata VALUES (?, ?)', [name, value])
-    out_cur.execute('CREATE TABLE tiles (zoom_level integer, tile_column integer, tile_row integer, tile_data blob)')
+    out_cur.execute('''CREATE TABLE tiles (
+        zoom_level integer,
+        tile_column integer,
+        tile_row integer,
+        tile_data blob
+    )''')
     if verbose: print('Done')
 
     # Update tiles from OS Servers
-    def flip_y(y):
-        """
-        https://gist.github.com/tmcw/4954720
-        """
-        return math.floor((2**16)-y-1)
-
     if verbose: print('Updating tiles from OS Servers (be patient!): ', end='', flush=True)
     for [z, x, y] in in_cur.execute('SELECT zoom_level, tile_column, tile_row FROM tiles'):
         if not out_cur.execute('SELECT count(*) FROM tiles WHERE zoom_level == ? AND tile_column == ? AND tile_row == ?', [z, x, y]).fetchone()[0]:
@@ -229,6 +289,8 @@ if __name__ == '__main__':
         extract(ARGS.file, ARGS.regions, ARGS.zoom, ARGS.verbose)
     elif ARGS.command == 'convert':
         convert(ARGS.file, ARGS.quality, ARGS.verbose)
+    elif ARGS.command == 'dedupe':
+        dedupe(ARGS.file1, ARGS.file2, ARGS.verbose)
     elif ARGS.command == 'update':
         if input('This scrapes tiles from OS Servers, which is antisocial, slow, and probably prohibited. Are you sure you want to continue? (Y/N): ').strip().upper() == 'Y':
             update(ARGS.file, ARGS.container, ARGS.delay, ARGS.verbose)
